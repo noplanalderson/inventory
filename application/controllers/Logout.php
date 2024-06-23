e6df93155fbd6f7164248576574d2436303f62bf
Muhammad Ridwan Na'im <noplanalderson@gmail.com>
2024-06-23 23:33:04 +0700

    Add logout feature

---
+++ application/controllers/Logout.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Logout extends CI_Controller
+{
+    public $access_control;
+
+    public function index()
+    {
+        if($this->access_control->isLogin()) redirect('login');
+        session_destroy();
+        redirect('login');
+    }
+}

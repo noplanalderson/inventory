3fc460f3d853c2683a1903942fe87473de8cc8af
Muhammad Ridwan Na'im <noplanalderson@gmail.com>
2024-06-23 21:52:09 +0700

    Add view condition for sidebar, topbar, and footer

--- application/views/components/layout.php
+++ application/views/components/layout.php
@@ -10,7 +10,7 @@
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
-    <link href="<?php echo base_url('assets/templates/css/sb-admin-2.min.css'); ?>" rel="stylesheet">
+    <link href="<?php echo base_url('assets/templates/css/sb-admin-2.css'); ?>" rel="stylesheet">
     <link href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap4.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/sweetalert2@10/dist/sweetalert2.min.css" rel="stylesheet">
     <style>
@@ -24,13 +24,13 @@
 
 <body id="page-top">
     <div id="wrapper">
-        <?php $this->load->view('components/sidebar'); ?>
+        <?php if($sidebar) $this->load->view('components/sidebar'); ?>
         <div id="content-wrapper" class="d-flex flex-column">
             <div id="content">
-                <?php $this->load->view('components/topbar'); ?>
+                <?php if($topbar) $this->load->view('components/topbar'); ?>
                 <?php $this->load->view($content); ?>
             </div>
-            <?php $this->load->view('components/footer'); ?>
+            <?php if($footer) $this->load->view('components/footer'); ?>
         </div>
     </div>
     <script src="<?php echo base_url('assets/templates/vendor/jquery/jquery.min.js'); ?>"></script>

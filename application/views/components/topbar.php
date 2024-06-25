3869edef4c585b2b50559c489b952b56d7353f86
Muhammad Ridwan Na'im <noplanalderson@gmail.com>
2024-06-25 10:27:55 +0700

    style(username): show current username

--- application/views/components/topbar.php
+++ application/views/components/topbar.php
@@ -7,7 +7,7 @@
         <li class="nav-item dropdown no-arrow">
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
                 aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Hai, NAMA</span>
+                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Hai, <?= sessionGet('username') ?></span>
             </a>
         </li>
 
